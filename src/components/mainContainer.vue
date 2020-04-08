01e42f204521631e2a6c7fb3160792ebd20def2b
Kubor <hebdajakub2aizst@gmail.com>
2020-04-08 16:25:11 +0200

    First try of implementing drag&drop. Have 2 errors

--- src/components/mainContainer.vue
+++ src/components/mainContainer.vue
@@ -13,8 +13,14 @@
     <div class="row">
       <div class="col-12 middle">
         <div class="file-section">
-          <div class="send-file">
+          <div class="send-file" v-cloak @drop.prevent="addFile" @dragover.prevent>
             <i class="fas fa-plus fa-5x"></i>
+            <ul> 
+              <li v-for="file in files">
+                {{ file.name  }} ({{ file.size | kb }} kb) <button @click="removeFile(file)" title="Remove">X</button>
+              </li>
+            </ul>
+            <button :disabled="uploadDisabled" @click="upload">Upload</button>
             <div class="regular-text">Drop the files here</div>
           </div>
         </div>
@@ -26,8 +32,50 @@
 <script>
 export default {
   name: 'mainContainer',
-  props: {
-    msg: String
+  data() {
+    files:[]
+  },
+
+  computed: {
+    uploadDisabled() {
+      return this.files.length === 0;
+    }
+  },
+
+  methods: {
+    addFile(e) {
+      let droppedFiles = e.dataTransfer.files;
+      if(!droppedFiles) return;
+      ([...droppedFiles]).forEach(f => {
+        this.files.push(f);
+      });
+    },
+
+    removeFile(file){
+      this.files = this.files.filter(f => {
+        return f != file;
+      });
+    },
+
+    upload() {
+
+      let formData = new FormData();
+      this.files.forEach((f,x) => {
+        formData.append('file'+(x+1), f);
+      });
+
+      fetch('https://httpbin.org/post', {
+        method:'POST',
+        body: formData
+      })
+      .then(res => res.json())
+      .then(res => {
+        console.log('done uploading', res);
+      })
+      .catch(e => {
+        console.error(JSON.stringify(e.message));
+      });
+    }
   }
 }
 </script>
